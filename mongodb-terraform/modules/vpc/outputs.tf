e15b374f1c8536d9af5645ea8198167704f80756
Mansoor Alam <90790553+meer727@users.noreply.github.com>
2025-06-28 23:43:19 +0530

    Update outputs.tf

--- mongodb-terraform/modules/vpc/outputs.tf
+++ mongodb-terraform/modules/vpc/outputs.tf
@@ -9,3 +9,13 @@ output "bastion_sg_id" {
 output "mongodb_sg_id" {
   value = aws_security_group.mongodb_sg.id
 }
+
+# Output Public Route Table ID
+output "public_route_table_id" {
+  value = aws_route_table.public_rt.id
+}
+
+# Output Internet Gateway ID
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
